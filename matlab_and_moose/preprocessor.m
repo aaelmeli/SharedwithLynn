2f600e0dbdd861a838590e7dbca77618da3e6a4e
Elmeliegy <elmeabde@sawtooth1.ib0.sawtooth.inl.gov>
2023-06-08 19:08:51 -0600

    modifying matlab-moose wrapper to work on INL-HPC

--- matlab_and_moose/preprocessor.m
+++ matlab_and_moose/preprocessor.m
@@ -11,85 +11,95 @@ forwardinputfilename=inputs.mooseforwardinputfilename;
 adjointinputfilename=inputs.mooseadjointinputfilename;
 gradientinputfilename=inputs.moosegradientinputfilename;
 spmd(length(inputs.freqs))
-    bashcmd = 'start /b bash -c '; % WSL
-    directoryname='_freq';
+    % directoryname='_freq';
+    % chr = int2str(labindex);
+    % directoryname=append(directoryname,chr); 
+    directoryname = '_freq';
     chr = int2str(labindex);
-    directoryname=append(directoryname,chr); 
-    
-        
-    %on windows
-    runcmd = ['" mkdir -p ' directoryname ' > NULL "'];
-    status = system([bashcmd runcmd]);
+    directoryname = append(directoryname, chr);
+    runcmd = [' mkdir -p  ' directoryname '  > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
-    end
-    
-    %on wsl
-    runcmd = ['" cd  /home/aaelmeli/projects/first_test && mkdir -p ' directoryname ' > NULL "'];
-    status = system([bashcmd runcmd]);
-    if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
 
     
+    %do the same in moose project
+    runcmd = [' cd /home/elmeabde/sawtooth1/projects/first_test && mkdir -p ' directoryname ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
+    if (status ~= 0)
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
+    end
+    
     %copy the executable into each of the directories, make sure that
-    %this executanle has been built using single processor
-    runcmd = ['" cd  /home/aaelmeli/projects/first_test && cp first_test-opt /home/aaelmeli/projects/first_test/' directoryname ' > NULL "'];
-    status = system([bashcmd runcmd]);
+    %this executable has been built using single processor
+    runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test && cp  first_test-opt  ' directoryname ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
-        runcmd = ['" cd  /home/aaelmeli/projects/first_test && cp ' sytheticinputfilename ' /home/aaelmeli/projects/first_test/' directoryname ' > NULL "'];
-    status = system([bashcmd runcmd]);
+    runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test && cp  ' sytheticinputfilename ' ' directoryname ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
-        runcmd = ['" cd  /home/aaelmeli/projects/first_test && cp ' forwardinputfilename ' /home/aaelmeli/projects/first_test/' directoryname ' > NULL "'];
-    status = system([bashcmd runcmd]);
+    runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test && cp  ' forwardinputfilename ' ' directoryname ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
-        runcmd = ['" cd  /home/aaelmeli/projects/first_test && cp ' adjointinputfilename ' /home/aaelmeli/projects/first_test/' directoryname ' > NULL "'];
-    status = system([bashcmd runcmd]);
+    runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test && cp  ' adjointinputfilename ' ' directoryname ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
-        runcmd = ['" cd  /home/aaelmeli/projects/first_test && cp ' gradientinputfilename ' /home/aaelmeli/projects/first_test/' directoryname ' > NULL "'];
-    status = system([bashcmd runcmd]);
+    runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test && cp ' gradientinputfilename ' ' directoryname ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
     %make directories, 
         %on wsl
         
     
-        runcmd = ['" cd  /home/aaelmeli/projects/first_test/' directoryname ' && mkdir -p ' syntheticdirectory ' > NULL "'];
-    status = system([bashcmd runcmd]);
+        runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test/' directoryname ' && mkdir -p ' syntheticdirectory ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
-        runcmd = ['" cd  /home/aaelmeli/projects/first_test/' directoryname ' && mkdir -p ' forwarddirectory ' > NULL "'];
-    status = system([bashcmd runcmd]);
+        runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test/' directoryname ' && mkdir -p ' forwarddirectory ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
-    runcmd = ['" cd  /home/aaelmeli/projects/first_test/' directoryname ' && mkdir -p ' adjointdirectory ' > NULL "'];
-    status = system([bashcmd runcmd]);
+    runcmd = [' cd  /home/elmeabde/sawtooth1/projects/first_test/' directoryname ' && mkdir -p ' adjointdirectory ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
     
-        runcmd = ['" cd  /home/aaelmeli/projects/first_test/' directoryname ' && mkdir -p ' gradientdirectory ' > NULL "'];
-    status = system([bashcmd runcmd]);
+        runcmd = [' cd /home/elmeabde/sawtooth1/projects/first_test/' directoryname ' && mkdir -p ' gradientdirectory ' > NULL '];
+    [status, cmderrmsg] = unix(runcmd);
+
     if (status ~= 0)
-        error(['Run bash cmd error: ' num2str(status)]);
+        error(['Run bash cmd error: ' num2str(status) ' ' cmderrmsg]);
     end
 end
 end
